cfa7aa1a2d947dce432354628f483d9ab9823976
Nanne Baars <nanneb@gmail.com>
2021-07-10 21:05:05 +0200

    Rewrite PAE so it no longer depends on okhttp Introduced commons library to share the class between version1 and 2

--- commons/src/test/java/org/paseto4j/commons/PreAuthenticationEncoderTest.java
+++ commons/src/test/java/org/paseto4j/commons/PreAuthenticationEncoderTest.java
@@ -22,25 +22,26 @@
  * SOFTWARE.
  */
 
-package org.paseto4j.version2;
+package org.paseto4j.commons;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.paseto4j.version2.Util;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.paseto4j.commons.PreAuthenticationEncoder.encode;
 
-class UtilTest {
+class PreAuthenticationEncoderTest {
 
     @Test
     void pae() {
         assertAll(
-                () -> assertEquals("0000000000000000", Util.pae(new String[]{})),
-                () -> assertEquals("01000000000000000000000000000000", Util.pae("")),
-                () -> assertEquals("020000000000000000000000000000000000000000000000", Util.pae("", "")),
-                () -> assertEquals("0100000000000000070000000000000050617261676f6e", Util.pae("Paragon")),
-                () -> assertEquals("0200000000000000070000000000000050617261676f6e0a00000000000000496e6974696174697665", Util.pae("Paragon", "Initiative")),
-                () -> assertEquals("0100000000000000190000000000000050617261676f6e0a00000000000000496e6974696174697665", Util.pae("Paragon\n\0\0\0\0\0\0\0Initiative"))
+                () -> Assertions.assertEquals("0000000000000000", HexToBytes.hexEncode(encode(new byte[][]{}))),
+                () -> Assertions.assertEquals("01000000000000000000000000000000", HexToBytes.hexEncode(encode("".getBytes(UTF_8)))),
+                () -> Assertions.assertEquals("020000000000000000000000000000000000000000000000", HexToBytes.hexEncode(encode("".getBytes(UTF_8), "".getBytes(UTF_8)))),
+                () -> Assertions.assertEquals("0100000000000000070000000000000050617261676f6e", HexToBytes.hexEncode(encode("Paragon".getBytes(UTF_8)))),
+                () -> Assertions.assertEquals("0200000000000000070000000000000050617261676f6e0a00000000000000496e6974696174697665", HexToBytes.hexEncode(encode("Paragon".getBytes(UTF_8), "Initiative".getBytes(UTF_8)))),
+                () -> Assertions.assertEquals("0100000000000000190000000000000050617261676f6e0a00000000000000496e6974696174697665", HexToBytes.hexEncode(encode("Paragon\n\0\0\0\0\0\0\0Initiative".getBytes(UTF_8))))
         );
     }
 
