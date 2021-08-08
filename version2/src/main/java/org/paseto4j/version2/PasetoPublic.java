23c7458c21f9b65064c411029e600a5abc65a517
Nanne Baars <nanneb@gmail.com>
2021-08-08 13:36:13 +0300

    Rename verify method to prevent overlap

--- version2/src/main/java/org/paseto4j/version2/PasetoPublic.java
+++ version2/src/main/java/org/paseto4j/version2/PasetoPublic.java
@@ -37,6 +37,7 @@ import static java.util.Base64.getUrlEncoder;
 import static java.util.Objects.requireNonNull;
 import static org.paseto4j.commons.ByteUtils.concat;
 import static org.paseto4j.commons.Conditions.isNullOrEmpty;
+import static org.paseto4j.commons.Conditions.verify;
 import static org.paseto4j.commons.PreAuthenticationEncoder.encode;
 
 class PasetoPublic {
@@ -52,8 +53,8 @@ class PasetoPublic {
     static String sign(PrivateKey privateKey, String payload, String footer) {
         requireNonNull(privateKey);
         requireNonNull(payload);
-        Conditions.verify(privateKey.hasLength(64), "key should be 32 bytes");
-        Conditions.verify(privateKey.isValidFor(Version.V2, Purpose.PURPOSE_PUBLIC), "Key is not valid for purpose and version");
+        verify(privateKey.hasLength(64), "key should be 32 bytes");
+        verify(privateKey.isValidFor(Version.V2, Purpose.PURPOSE_PUBLIC), "Key is not valid for purpose and version");
 
         byte[] m2 = encode(PUBLIC.getBytes(UTF_8), payload.getBytes(UTF_8), footer.getBytes(UTF_8));
         byte[] signature = Signature.signDetached(m2, Signature.SecretKey.fromBytes(privateKey.material));
@@ -72,18 +73,18 @@ class PasetoPublic {
     static String parse(PublicKey publicKey, String signedMessage, String footer) throws SignatureException {
         requireNonNull(publicKey);
         requireNonNull(signedMessage);
-        Conditions.verify(publicKey.hasLength(32), "key should be 32 bytes");
-        Conditions.verify(publicKey.isValidFor(Version.V2, Purpose.PURPOSE_PUBLIC), "Key is not valid for purpose and version");
+        verify(publicKey.hasLength(32), "key should be 32 bytes");
+        verify(publicKey.isValidFor(Version.V2, Purpose.PURPOSE_PUBLIC), "Key is not valid for purpose and version");
 
         String[] tokenParts = signedMessage.split("\\.");
 
         //1
         if (!isNullOrEmpty(footer)) {
-            Conditions.verify(MessageDigest.isEqual(getUrlDecoder().decode(tokenParts[3]), footer.getBytes(UTF_8)), "footer does not match");
+            verify(MessageDigest.isEqual(getUrlDecoder().decode(tokenParts[3]), footer.getBytes(UTF_8)), "footer does not match");
         }
 
         //2
-        Conditions.verify(signedMessage.startsWith(PUBLIC), "Token should start with " + PUBLIC);
+        verify(signedMessage.startsWith(PUBLIC), "Token should start with " + PUBLIC);
 
         //3
         byte[] sm = getUrlDecoder().decode(tokenParts[2]);
@@ -94,12 +95,12 @@ class PasetoPublic {
         byte[] m2 = PreAuthenticationEncoder.encode(PUBLIC.getBytes(UTF_8), message, footer.getBytes(UTF_8));
 
         //5
-        verify(publicKey, m2, signature);
+        verifySignature(publicKey, m2, signature);
 
         return new String(message);
     }
 
-    private static void verify(PublicKey key, byte[] message, byte[] signature) throws SignatureException {
+    private static void verifySignature(PublicKey key, byte[] message, byte[] signature) throws SignatureException {
         boolean valid = Signature.verifyDetached(message, signature, Signature.PublicKey.fromBytes(key.material));
         if (!valid) {
             throw new SignatureException("Invalid signature");
