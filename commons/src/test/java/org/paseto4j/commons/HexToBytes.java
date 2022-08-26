5c5a0bb60f4d307b2ab912f324ac77cbc74a905c
Nanne Baars <nanneb@gmail.com>
2022-08-26 14:15:16 +0200

    Update poms and add checks and remove Guava dependency

--- commons/src/test/java/org/paseto4j/commons/HexToBytes.java
+++ commons/src/test/java/org/paseto4j/commons/HexToBytes.java
@@ -1,14 +1,46 @@
 package org.paseto4j.commons;
 
-import com.google.common.io.BaseEncoding;
-
+/**
+ * Copied from
+ * http://www.docjar.com/html/api/com/sun/xml/internal/bind/DatatypeConverterImpl.java.html
+ */
 public class HexToBytes {
 
-  public static byte[] hexToBytes(String hex) {
-    return BaseEncoding.base16().lowerCase().decode(hex);
+  public static byte[] hexToBytes(String s) {
+    final int len = s.length();
+
+    // "111" is not a valid hex encoding.
+    if (len % 2 != 0) throw new IllegalArgumentException("hexBinary needs to be even-length: " + s);
+
+    byte[] out = new byte[len / 2];
+
+    for (int i = 0; i < len; i += 2) {
+      int h = hexToBin(s.charAt(i));
+      int l = hexToBin(s.charAt(i + 1));
+      if (h == -1 || l == -1)
+        throw new IllegalArgumentException("contains illegal character for hexBinary: " + s);
+
+      out[i / 2] = (byte) (h * 16 + l);
+    }
+
+    return out;
   }
 
-  public static String hexEncode(byte[] bytes) {
-    return BaseEncoding.base16().lowerCase().encode(bytes);
+  private static int hexToBin(char ch) {
+    if ('0' <= ch && ch <= '9') return ch - '0';
+    if ('A' <= ch && ch <= 'F') return ch - 'A' + 10;
+    if ('a' <= ch && ch <= 'f') return ch - 'a' + 10;
+    return -1;
+  }
+
+  private static final char[] hexCode = "0123456789abcdef".toCharArray();
+
+  public static String hexEncode(byte[] data) {
+    StringBuilder r = new StringBuilder(data.length * 2);
+    for (byte b : data) {
+      r.append(hexCode[(b >> 4) & 0xF]);
+      r.append(hexCode[(b & 0xF)]);
+    }
+    return r.toString();
   }
 }
